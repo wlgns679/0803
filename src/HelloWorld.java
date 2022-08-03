36b58ce6939c32291b8cec0fd6477149f688f094
kimmincheols <look753@naver.com>
2022-08-03 09:45:24 +0900

    협

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello, Wolrdㅎ");
+		System.out.println("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");
 	}
 }
